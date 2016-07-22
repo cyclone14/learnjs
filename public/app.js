c391bcbaff6f49f05065b551be8afbeab911990b
John Price <jprice1947@comcast.net>
2016-07-22 12:30:34 -0400

    fix typo
    AWS S3 test and production buckets

--- public/app.js
+++ public/app.js
@@ -2,5 +2,5 @@
 var learnjs = {};
 learnjs.showView = function(hash) {
     var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view.container').empty().append(problemView);
+    $('.view-container').empty().append(problemView);
 }
