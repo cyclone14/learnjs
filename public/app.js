7e38a75b91604e6afb67493128162d51d8cfaf2b
John Price <jprice1947@comcast.net>
2016-08-05 21:42:40 -0400

    Fade in/out for the answer response

--- public/app.js
+++ public/app.js
@@ -14,9 +14,9 @@ learnjs.problemView = function(data) {
 
     function checkAnswerClick() {
 	if (checkAnswer()) {
-	    resultFlash.text('Correct!');
+	    learnjs.flashElement(resultFlash, 'Correct!');
 	} else {
-	    resultFlash.text('Incorrect!');
+	    learnjs.flashElement(resultFlash, 'Incorrect!');
 	}
 	return false;
     }
@@ -36,6 +36,13 @@ learnjs.showView = function(hash) {
     if (viewFn) {
 	$('.view-container').empty().append(viewFn(hashParts[1]));
     }
+}
+
+learnjs.flashElement = function(elem, content) {
+    elem.fadeOut('fast', function() {
+        elem.html(content);
+        elem.fadeIn();
+    })
 }
 
 learnjs.appOnReady = function() {
