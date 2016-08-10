7deef079f79e13ae1d21680595b203459ad317c9
John Price <jprice1947@comcast.net>
2016-08-09 21:57:58 -0400

    pre Application Shell

--- public/app.js
+++ public/app.js
@@ -14,7 +14,8 @@ learnjs.problemView = function(data) {
 
     function checkAnswerClick() {
 	if (checkAnswer()) {
-	    learnjs.flashElement(resultFlash, 'Correct!');
+	    var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+	    learnjs.flashElement(resultFlash, correctFlash);
 	} else {
 	    learnjs.flashElement(resultFlash, 'Incorrect!');
 	}
@@ -27,6 +28,10 @@ learnjs.problemView = function(data) {
     return view;
 }
 
+learnjs.template = function(name) {
+    return $('.templates .' + name).clone();
+}
+
 learnjs.showView = function(hash) {
     var routes = {
 	'#problem': learnjs.problemView
@@ -36,6 +41,18 @@ learnjs.showView = function(hash) {
     if (viewFn) {
 	$('.view-container').empty().append(viewFn(hashParts[1]));
     }
+}
+
+learnjs.buildCorrectFlash = function(problemNum) {
+    var correctFlash = learnjs.template('correct-flash');
+    var link = correctFlash.find('a');
+    if (problemNum < learnjs.problems.length) {
+        link.attr('href', '#problem-' + (problemNum + 1));
+    } else {
+        link.attr('href', '');
+        link.text("You're Finished");
+    }
+    return correctFlash;
 }
 
 learnjs.flashElement = function(elem, content) {
