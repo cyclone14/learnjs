b402baba0013ae00f39d2dcc359013b3a04aa3d2
John Price <jprice1947@comcast.net>
2017-01-31 21:41:19 -0500

    first part of using database to track progress

--- public/app.js
+++ public/app.js
@@ -161,6 +161,45 @@ learnjs.triggerEvent = function(name, args) {
     $('.view-container>*').trigger(name, args);
 };
 
+learnjs.sendDbRequest = function(req, retry) {
+    var promise = new $.Deferred();
+    req.on('error', function(error) {
+        if (error.code === "CredentialsError") {
+            learnjs.identity.then(function(identity) {
+                return identity.refresh().then(function() {
+                    return retry();
+                }, function () {
+                    promise.reject(resp);
+                });
+            });
+        } else {
+            promise.reject(error);
+        }
+    });
+    req.on('success', function(resp) {
+        promise.resolve(resp.data);
+    });
+    req.send();
+    return promise;
+}
+
+learnjs.saveAnswer = function (problemId, answer) {
+    return learnjs.identity.then(function (identity) {
+        var db = new AWS.DynamoDB.DocumentClient();
+        var item = {
+            TableName: 'learnjs',
+            Item: {
+                userId: identity.id,
+                problemId: problemId,
+                answer: answer
+            }
+        };
+        return learnjs.sendDbRequest(db.put(item), function() {
+            return learnjs.saveAnswer(problemId, answer);
+        })
+    });
+}
+
 learnjs.problems = [
     {
         description: "What is truth?",
